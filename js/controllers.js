9a501907ce5fead616389c3673e681da1fc2c12e
ranelpro <ranel@devoremedia.com>
2014-08-07 18:49:53 +0300

    AJS 1.02

--- js/controllers.js
+++ js/controllers.js
@@ -4,7 +4,7 @@ var myApp = angular.module('myApp', []);
 myApp.controller('MyController', function MyController ($scope) {
 	$scope.author = {
 		'name' : 'Ranel T.',
-		'title' : 'Front-End Developer',
+		'title' : 'Front-End Developer / Graphic Designer',
 		'company' : 'DeVore Media'
 	}
 });
